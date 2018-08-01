9efcf2e425473e74c6b54d2230423751585a14b6
Jakub Flaska <jakub.flaska@nih.gov>
2018-08-01 17:48:30 -0400

    x

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const express = require('express'),
-    compression = require('compression');
-const app = express();
+    compression = require('compression'),
+    mail = require('./server/mail')
+    app = express();
 
-app.use(express.static('public'));
 
 app.use(compression());
+
+app.post('/email', function(req, res){
+
+});
+
+
+app.use(express.static('public'));
 
 var appPort = process.env.PORT || 3003;
 
