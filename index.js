84b79d920203ba0866ddd6954f6f450148f1e1a6
Jakub Flaska <jakub.flaska@nih.gov>
2018-08-02 16:32:03 -0400

    x

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
 const express = require('express'),
     compression = require('compression'),
-    mail = require('./server/mail')
+    mail = require('./server/mail'),
+    bodyParser = require('body-parser')
     app = express();
+
+function genericResponseFactory(req, res){
+    return function genericResponse(err, result) {
+        if (err) {
+            console.log(err);
+            return res.status(500).send(err);
+        }
+        if (result===null) return res.status(400).send("No result returned.");
+        res.send(result);
+    }
+};
 
 
 app.use(compression());
+app.use(bodyParser.urlencoded({ extended: false }))
 
-app.post('/email', function(req, res){
-
+app.post('/email/submit', function(req, res){
+    mail.forwardMessage(req.body, genericResponseFactory(req, res));
 });
-
 
 app.use(express.static('public'));
 
--- server/mail.js
+++ server/mail.js
@@ -1,40 +1,23 @@
 const nodemailer = require('nodemailer');
 
-exports.forwardMessage = function(){
-
-};
-
-nodemailer.createTestAccount((err, account) => {
-    // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        host: 'smtp.ethereal.email',
-        port: 587,
-        secure: false, // true for 465, false for other ports
-        auth: {
-            user: account.user, // generated ethereal user
-            pass: account.pass // generated ethereal password
-        }
-    });
-
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Fred Foo 👻" <foo@example.com>', // sender address
-        to: 'bar@example.com, baz@example.com', // list of receivers
-        subject: 'Hello ✔', // Subject line
-        text: 'Hello world?', // plain text body
-        html: '<b>Hello world?</b>' // html body
-    };
-
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        // Preview only available when sending through an Ethereal account
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
-    });
+const transporter = nodemailer.createTransport({
+    host: 'smtp.gmail.com',
+    port: 587,
+    secure: false,
+    auth: {
+        user: 'jacob.code.house@gmail.com',
+        pass: 'Green18!'
+    }
 });
+
+
+exports.forwardMessage = function(message, cb){
+    const mailOptions = {
+        from: '"Code House Website" <jacob.code.house@gmail.com>', // sender address
+        to: 'jakub.flaska@gmail.com', // list of receivers
+        subject: 'Code House | Client Request', // Subject line
+        text: 'Hello world', // plain text body
+        html: "<b>Name</b> " + message.name + "<br><b>Email</b> " + message.email + "<br><b>Phone</b> " + message.phone + "<br><b>Message</b>" + message.message
+    };
+    transporter.sendMail(mailOptions, cb);
+};
